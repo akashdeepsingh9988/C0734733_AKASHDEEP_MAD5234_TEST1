d898b32ca8bbe714c7a3e6f30b4250e6e6225440
Akashdeep Singh <akashthind007@gmail.com>
2019-05-16 11:37:50 -0400

    Create TestBlazeDemo.java

---
+++ C0734733_AKASHDEEP_MAD5234_TEST1/test/TestBlazeDemo.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author Akashdeep Singh
+ */
+public class TestBlazeDemo {
+
+    public TestBlazeDemo() {
+    }
+
+    final String URL = "https://www.blazedemo.com/";
+    final String DRIVER_PATH = "C:\\chromedriver.exe";
+
+    WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        // Setup Selenium + Chrome
+        System.setProperty("webdriver.chrome.driver", DRIVER_PATH);
+        driver = new ChromeDriver();
+        driver.get(URL);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Thread.sleep(1000);				// OPTION 1
+        TimeUnit.SECONDS.sleep(1);		// OPTION 2
+        driver.close();
+    }
+
+    
+        @Test
+    public void testNumberofLinks() {
+
+
+    @Test
+    public void testNumberofLinks() {
+
+        // Get the links in the list
+        List<WebElement> listOfLinks = driver.findElements(By.cssSelector("table+ul li a"));
+
+        // Count the number of links
+        int numLinks = listOfLinks.size();
+
+        // Check if number of links = 10
+        assertEquals(10, numLinks);
+
+    }
+
+}
